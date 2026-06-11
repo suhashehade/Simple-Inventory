4a3daf8692951978be929f08d8c1ed533a674fe1
Suha Shehadeh <suha.shehadeh@foothillsolutions.com>
2026-06-11 11:50:17 -0700

    feat: add insert product

--- classes/Inventory.cs
+++ classes/Inventory.cs
@@ -16,11 +16,6 @@ public class Inventory
             return _instatnce;
         }
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 0c04885 (feat: create inventory & product classes)
     
     public static void AddProduct(Product product) {
         Products.Add(product);
@@ -29,25 +24,5 @@ public class Inventory
         Console.ResetColor();
     }
 
-<<<<<<< HEAD
-=======
->>>>>>> 926b64f (feat: create inventory & product classes)
-=======
-    public static void DisplayProducts() {
-        if(Products.Count == 0)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("No products to display");
-            Console.ResetColor();
-        }
-        Console.WriteLine("{0,-10} {1,-10} {2,-10}", "Name", "Price", "Quantity");
-        Console.WriteLine("------------------------------------");
-        foreach (var p in Products)
-            {
-                Console.WriteLine($"{ p.Name,-10 } { p.Price,-10 } { p.Quantity,-10 }");
-            }
-    }
->>>>>>> 7269152 (feat: create inventory & product classes)
->>>>>>> 0c04885 (feat: create inventory & product classes)
 
 }
