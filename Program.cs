ca354d596dfc3402c3ef1830060f34027ac2ddf5
Suha Shehadeh <suha.shehadeh@foothillsolutions.com>
2026-06-11 11:42:21 -0700

    feat: add insert product

--- Program.cs
+++ Program.cs
@@ -10,3 +10,48 @@ Console.WriteLine("""
         ---------------------------------------
         """);
 string? input = Console.ReadLine();
+while(true)
+{
+    switch (input?.ToLower())
+    {
+        case "a":
+            {
+                Console.WriteLine("""
+                I will ask you to enter the name, price, and quantitiy of the product.
+                First of all, enter the name of the product ...
+                """);
+                string? nameInput = Console.ReadLine();
+                string name = nameInput!;
+                while (name == "")
+                {
+                    Console.WriteLine("Invalid Name 😞💔");
+                    nameInput = Console.ReadLine();
+                    name = nameInput!;
+                }
+                Console.WriteLine("Now, enter the price of the product:");
+                string? priceInput = Console.ReadLine();
+                double price;
+                while (!double.TryParse(priceInput, out price))
+                {
+                    Console.WriteLine("Invalid Price 😞💔");
+                    priceInput = Console.ReadLine();
+                }
+
+                Console.WriteLine("Finally, enter the quantity of the product:");
+                string? quantitiyInput = Console.ReadLine();
+                int quanitiy;
+                while (!int.TryParse(quantitiyInput, out quanitiy))
+                {
+                    Console.WriteLine("Invalid quantitiy 😞💔");
+                    quantitiyInput = Console.ReadLine();
+                }
+
+                Product product = new Product(name, price, quanitiy);
+                Inventory.AddProduct(product);
+                break;
+            }
+        
+        
+    }
+    input = Console.ReadLine();
+}
--- classes/Inventory.cs
+++ classes/Inventory.cs
@@ -24,19 +24,5 @@ public class Inventory
         Console.ResetColor();
     }
 
-    public static void DisplayProducts() {
-        if(Products.Count == 0)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("No products to display");
-            Console.ResetColor();
-        }
-        Console.WriteLine("{0,-10} {1,-10} {2,-10}", "Name", "Price", "Quantity");
-        Console.WriteLine("------------------------------------");
-        foreach (var p in Products)
-            {
-                Console.WriteLine($"{ p.Name,-10 } { p.Price,-10 } { p.Quantity,-10 }");
-            }
-    }
 
 }
