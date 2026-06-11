726915231da8c59a30c979f122d13a6528584e8e
Suha Shehadeh <suha.shehadeh@foothillsolutions.com>
2026-06-11 11:14:46 -0700

    feat: create inventory & product classes

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Collections;
+
+Console.WriteLine("""
+        Insructions:
+        Press A to add product
+        Press R to display the products
+        Press U to edit product
+        Press D to delete product
+        Press Q to Exit application
+        ---------------------------------------
+        """);
+string? input = Console.ReadLine();
---
+++ classes/Inventory.cs
@@ -0,0 +1,42 @@
+using System.ComponentModel;
+
+public class Inventory
+{
+    public static List<Product> Products = new List<Product>();
+    private static Inventory? _instatnce;
+    private Inventory(){}
+    public static Inventory? Instance
+    {
+        get
+        { 
+            if(_instatnce == null)
+            {
+                _instatnce = new Inventory();
+            }
+            return _instatnce;
+        }
+    }
+    
+    public static void AddProduct(Product product) {
+        Products.Add(product);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("The product added successfully ✔️✔️");
+        Console.ResetColor();
+    }
+
+    public static void DisplayProducts() {
+        if(Products.Count == 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("No products to display");
+            Console.ResetColor();
+        }
+        Console.WriteLine("{0,-10} {1,-10} {2,-10}", "Name", "Price", "Quantity");
+        Console.WriteLine("------------------------------------");
+        foreach (var p in Products)
+            {
+                Console.WriteLine($"{ p.Name,-10 } { p.Price,-10 } { p.Quantity,-10 }");
+            }
+    }
+
+}
